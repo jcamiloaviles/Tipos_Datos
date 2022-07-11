7d1630764c7fbafb4987a33213dcf1f98c363180
jcamiloaviles <jcamiloaviles@gmail.com>
2022-07-11 16:00:04 -0500

    Se agrega el proyecto

---
+++ src/HolaMundo.java
@@ -0,0 +1,17 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        String saludar="Hola, como estan";
+        System.out.println(saludar + " Primer proyecto");
+        System.out.println(saludar.toUpperCase());
+        int numero= 10;
+        System.out.println("Es un numero entero:" + numero);
+        float numeroDecimal= 12.02f;
+        System.out.println("Es un numero flotante:" + numeroDecimal);
+        double numeroDouble= 12.25;
+        System.out.println("Es un numero Double:" + numeroDouble);
+        boolean valorT= true;
+        boolean valorF= false;
+        System.out.println("True es un valor Booleano:" + valorT);
+        System.out.println("False es un valor Booleano:" + valorF);
+    }
+}
